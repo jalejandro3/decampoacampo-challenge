6aff3a6844c2fbc3efef5738447a3342748d7d10
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-08-28 17:29:52 -0500

    Crear bootstrap con cableado de dependencias

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+use Dotenv\Dotenv;
+use Jalejandro\DecampoacampoChallenge\Application\MostrarProducto;
+use Jalejandro\DecampoacampoChallenge\Infrastructure\EnvConfig;
+use Jalejandro\DecampoacampoChallenge\Infrastructure\PDOProductoRepository;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = new EnvConfig([
+    'PRECIO_USD' => $_ENV['PRECIO_USD'] ?? null,
+    'DB_HOST' => $_ENV['DB_HOST'] ?? null,
+    'DB_NAME' => $_ENV['DB_NAME'] ?? null,
+    'DB_USER' => $_ENV['DB_USER'] ?? null,
+    'DB_PASS' => $_ENV['DB_PASS'] ?? '',
+    'DB_PORT' => $_ENV['DB_PORT'] ?? null,
+]);
+
+$dsn = "mysql:host={$config->getDbHost()};port={$config->getDbPort()};dbname={$config->getDbName()};charset=utf8mb4";
+$pdo = new PDO($dsn, $config->getDbUser(), $config->getDbPass(), [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$repositorio = new PDOProductoRepository($pdo);
+$mostrarProducto = new MostrarProducto($config, $repositorio);
+
+return $mostrarProducto;

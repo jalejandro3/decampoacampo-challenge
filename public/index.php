5f44bdaf41d5404723dcd63ecd07ac9e85270159
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-08-30 13:38:38 -0500

    Refactor del index.php para capturar excepciones del PDO y base de datos

--- public/index.php
+++ public/index.php
@@ -6,14 +6,16 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
 require __DIR__ . '/../vendor/autoload.php';
-$controllers = require __DIR__ . '/../bootstrap.php';
-$routes = require __DIR__ . '/../config/routes.php';
 
 $request = Request::createFromGlobals();
-$context = new RequestContext()->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
 
 try {
+    $controllers = require __DIR__ . '/../bootstrap.php';
+    $routes = require __DIR__ . '/../config/routes.php';
+
+    $context = new RequestContext()->fromRequest($request);
+    $matcher = new UrlMatcher($routes, $context);
+
     $parameters = $matcher->match($request->getPathInfo());
     $controller = $controllers[$parameters['_controller']];
     $routeParams = array_filter($parameters, fn($key) => !str_starts_with($key, '_'), ARRAY_FILTER_USE_KEY);

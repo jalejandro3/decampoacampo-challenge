29868729962c232b8a66b112f9fd2d21cd33f473
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-08-30 15:35:50 -0500

    Agregar pruebas unitarias a DatoRequeridoException y DatoInvalidoException

--- tests/Unit/Http/ExceptionHandlerTest.php
+++ tests/Unit/Http/ExceptionHandlerTest.php
@@ -2,6 +2,8 @@
 
 namespace Unit\Http;
 
+use Jalejandro\DecampoacampoChallenge\Exception\DatoInvalidoException;
+use Jalejandro\DecampoacampoChallenge\Exception\DatoRequeridoException;
 use Jalejandro\DecampoacampoChallenge\Exception\ProductoNoEncontradoException;
 use Jalejandro\DecampoacampoChallenge\Http\ExceptionHandler;
 use PHPUnit\Framework\TestCase;
@@ -39,5 +41,25 @@ class ExceptionHandlerTest extends TestCase
 
         $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
         $this->assertEquals(json_encode(['error' => 'Recurso no encontrado']), $response->getContent());
+    }
+
+    public function test_exception_handler_excepcion_dato_requerido_exception()
+    {
+        $exception = new DatoRequeridoException('Datos requeridos incompletos');
+        $exceptionHandler = new ExceptionHandler();
+        $response = $exceptionHandler->handle($exception);
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        $this->assertEquals(json_encode(['error' => 'Datos requeridos incompletos']), $response->getContent());
+    }
+
+    public function test_exception_handler_excepcion_dato_invalido_exception()
+    {
+        $exception = new DatoInvalidoException('El nombre del producto no puede estar vacío.');
+        $exceptionHandler = new ExceptionHandler();
+        $response = $exceptionHandler->handle($exception);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $this->assertEquals(json_encode(['error' => 'El nombre del producto no puede estar vacío.']), $response->getContent());
     }
 }

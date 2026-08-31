f2b0d808ead8a5125d1c0f644eb1d1f996c22315
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-08-30 19:58:23 -0500

    Refactorizar ActualizarProductoTest para evaluar logica del caso de uso de manera correcta

--- tests/Unit/Application/ActualizarProductoTest.php
+++ tests/Unit/Application/ActualizarProductoTest.php
@@ -13,42 +13,41 @@ class ActualizarProductoTest extends TestCase
 {
     public function test_actualizar_producto()
     {
-        $productoRepository = $this->createStub(ProductoRepository::class);
+        $productoRepository = $this->createMock(ProductoRepository::class);
         $configuracion = $this->createStub(Configuracion::class);
         $producto = new Producto(1, 'Cerdo', 'Lechon', 50000.0);
 
         $productoRepository->method('findById')->willReturn($producto);
-        $productoRepository->method('update');
+
+        $productoRepository
+            ->expects($this->once())
+            ->method('update')
+            ->with($this->callback(function (Producto $productoActualizado) {
+                return $productoActualizado->getId() === 1 &&
+                    $productoActualizado->getNombre() === 'Ganado' &&
+                    $productoActualizado->getDescripcion() === 'Maute' &&
+                    $productoActualizado->getPrecio() === 1000.0;
+            }));
+
         $configuracion->method('getPrecioUsd')->willReturn(1000.0);
 
         $actualizarProducto = new ActualizarProducto($configuracion, $productoRepository);
         $resultado = $actualizarProducto->execute(1, 'Ganado', 'Maute', 1000.0);
 
-        $this->assertCount(5, $resultado);
-
-        $this->assertArrayHasKey('id', $resultado);
-        $this->assertArrayHasKey('nombre', $resultado);
-        $this->assertArrayHasKey('descripcion', $resultado);
-        $this->assertArrayHasKey('precio', $resultado);
-        $this->assertArrayHasKey('precio_usd', $resultado);
-
-        $this->assertSame('Ganado', $resultado['nombre']);
-        $this->assertSame('Maute', $resultado['descripcion']);
         $this->assertSame(1000.0, $resultado['precio']);
-        $this->assertSame(1.0, $resultado['precio_usd']);
     }
 
-    public function test_actualizar_producto_con_id_no_existente_retorna_producto_no_encontrado_exception()
+    public function test_actualizar_producto_con_id_no_existente_propaga_exception_y_no_actualiza_producto()
     {
-        $productoRepository = $this->createStub(ProductoRepository::class);
+        $productoRepository = $this->createMock(ProductoRepository::class);
         $configuracion = $this->createStub(Configuracion::class);
 
         $productoRepository->method('findById')->willReturn(null);
+        $productoRepository->expects($this->never())->method('update');
 
         $actualizarProducto = new ActualizarProducto($configuracion, $productoRepository);
 
         $this->expectException(ProductoNoEncontradoException::class);
-        $this->expectExceptionMessageIs('El producto con id 1 no existe.');
 
         $actualizarProducto->execute(1, 'Ganado', 'Maute', 1000.0);
     }

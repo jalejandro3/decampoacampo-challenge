ed496ebf44e1591b5dc8d7de078d7562424a7337
Jose Alejandro Alvarez Mendoza <alvarez.josealejandro3@gmail.com>
2026-08-30 20:16:59 -0500

    Refatorizacion de ActualizarProductoTest para cubrir los tres caminos de actualizacion

--- tests/Unit/Application/ActualizarProductoTest.php
+++ tests/Unit/Application/ActualizarProductoTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Application;
 
 use Jalejandro\DecampoacampoChallenge\Application\ActualizarProducto;
 use Jalejandro\DecampoacampoChallenge\Application\Configuracion;
+use Jalejandro\DecampoacampoChallenge\Exception\DatoInvalidoException;
 use Jalejandro\DecampoacampoChallenge\Exception\ProductoNoEncontradoException;
 use Jalejandro\DecampoacampoChallenge\Model\Producto;
 use Jalejandro\DecampoacampoChallenge\Model\ProductoRepository;
@@ -37,7 +38,7 @@ class ActualizarProductoTest extends TestCase
         $this->assertSame(1000.0, $resultado['precio']);
     }
 
-    public function test_actualizar_producto_con_id_no_existente_propaga_exception_y_no_actualiza_producto()
+    public function test_actualizar_producto_con_id_no_existente_propaga_excepcion()
     {
         $productoRepository = $this->createMock(ProductoRepository::class);
         $configuracion = $this->createStub(Configuracion::class);
@@ -48,7 +49,31 @@ class ActualizarProductoTest extends TestCase
         $actualizarProducto = new ActualizarProducto($configuracion, $productoRepository);
 
         $this->expectException(ProductoNoEncontradoException::class);
+        $this->expectExceptionMessageIs('El producto con id 1 no existe.');
 
         $actualizarProducto->execute(1, 'Ganado', 'Maute', 1000.0);
+    }
+
+    public function test_actualizar_producto_con_datos_invalidos_propaga_excepcion_y_no_actualiza()
+    {
+        $productoRepository = $this->createMock(ProductoRepository::class);
+        $configuracion = $this->createStub(Configuracion::class);
+        $producto = new Producto(1, 'Cerdo', 'Lechon', 50000.0);
+
+        $productoRepository
+            ->expects($this->once())
+            ->method('findById')
+            ->with(1)
+            ->willReturn($producto);
+
+        $productoRepository
+            ->expects($this->never())
+            ->method('update');
+
+        $actualizarProducto = new ActualizarProducto($configuracion, $productoRepository);
+
+        $this->expectException(DatoInvalidoException::class);
+
+        $actualizarProducto->execute(1, '', 'Maute', 1000.0);
     }
 }
